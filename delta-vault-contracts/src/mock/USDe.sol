8a7bc466f8b89ed21e15dcc46ba4fd5b42001eaf
startup-dreamer <prsumit35@gmail.com>
2024-11-23 05:25:08 +0530

    chore: removing chainlink infra and added mock USDe.sol

--- delta-vault-contracts/src/mock/USDe.sol
+++ delta-vault-contracts/src/mock/USDe.sol
@@ -5,15 +5,18 @@ import "solady/tokens/ERC20.sol";
 
 contract USD is ERC20 {
     function name() public pure override returns (string memory) {
-        return "USD";
+        return "USDe";
     }
 
     /// @dev Returns the symbol of the token.
     function symbol() public pure override returns (string memory) {
-        return "U";
+        return "USDe";
     }
 
     function mint(address user, uint256 amount) public {
         _mint(user, amount);
     }
 }
+
+
+// 0x5FbDB2315678afecb367f032d93F642f64180aa3 link token address added
